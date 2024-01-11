4a0182e50f54cd3c5e43932cc105a7ad2134320a
krla22 <kurtrobin22@gmail.com>
2024-01-11 15:47:33 +0800

    Job Finder App

---
+++ hook/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+          'X-RapidAPI-Key': '1a3b944ff0msh953650d94599ab1p124b3cjsn38c819c6445c',
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query }
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true)
+        try {
+            const response = await axios.request
+            (options);
+
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert(error)
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return{ data, isLoading, error, refetch }
+}
+
+export default useFetch;
